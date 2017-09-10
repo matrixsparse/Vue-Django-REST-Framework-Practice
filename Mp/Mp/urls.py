23e49782a0eea2ee824f329b2b7b18a7ee62a804
sparsematrix <matrix@sparsematrix.local>
2017-09-11 07:11:09 +0800

    update code

--- Mp/Mp/urls.py
+++ Mp/Mp/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from Mp.settings import MEDIA_ROOT
+from django.views.static import serve
+import xadmin
+
 # from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^xadmin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT})
 ]
